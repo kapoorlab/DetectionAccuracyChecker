9197ab22c285747d2ecd4a2d5edc080517f69027
kapoorlab <randomaccessiblekapoor@gmail.com>
2019-12-16 14:40:34 +0100

    Human vs AI accuracy checker

--- src/main/java/csvImporter/RenderPoints.java
+++ src/main/java/csvImporter/RenderPoints.java
@@ -7,10 +7,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ij.ImageJ;
+import net.imglib2.KDTree;
 import net.imglib2.RealPoint;
 
 public class RenderPoints {
 
+	
+	
+	public static double CheckMaster(List<RealPoint> TrueDots, List<RealPoint> QuestionDots, double distanceThreshold, int timeThreshold) {
+		
+		double accuracy = 0;
+		
+		double size = Math.min(TrueDots.size(), QuestionDots.size());
+		
+		for (RealPoint targetpoint: QuestionDots) {
+		
+		RealPoint Nearest = getNearestPoint(TrueDots, targetpoint);
+		
+		double GTX = Nearest.getDoublePosition(0);
+		double GTY = Nearest.getDoublePosition(1);
+		int GTtime = (int) Nearest.getDoublePosition(2); 
+		
+		double [] Truespace = new double[] {GTX, GTY};
+		
+		double [] Queryspace = new double[] {targetpoint.getDoublePosition(0), targetpoint.getDoublePosition(1)};
+		
+		int Querytime = (int)targetpoint.getDoublePosition(2);
+		
+		if (Math.abs(Querytime - GTtime) <= timeThreshold) {
+		
+			double distance = GetSpaceDistance(Truespace, Queryspace);
+				if (distance < distanceThreshold ) {
+					
+					accuracy+= 1;
+				}
+		}
+		
+		}
+		return accuracy/size;
+		
+	}
+	
+	public static double GetSpaceDistance(double[] source, double[] target) {
+		
+		double distance = 0;
+		
+		for (int dim = 0; dim < source.length; dim++) {
+			
+			distance += (source[dim] - target[dim]) * (source[dim] - target[dim]);
+			
+		}
+		
+		return Math.sqrt(distance);
+	}
 	
 	public static List<RealPoint> loadPoints3dCsv( String csvPath ) throws IOException
 	{
@@ -25,20 +74,70 @@ public class RenderPoints {
 		}
 		return pts;
 	}
+	public static RealPoint getNearestPoint(List<RealPoint> Allrois, RealPoint Clickedpoint) {
 
+		RealPoint KDtreeroi = null;
+
+		final List<RealPoint> targetCoords = new ArrayList<RealPoint>(Allrois.size());
+		final List<FlagNode<RealPoint>> targetNodes = new ArrayList<FlagNode<RealPoint>>(Allrois.size());
+		for (int index = 0; index < Allrois.size(); ++index) {
+
+			RealPoint r = Allrois.get(index);
+			 
+			 targetCoords.add( r );
+			 
+
+			targetNodes.add(new FlagNode<RealPoint>(Allrois.get(index)));
+
+		}
+
+		if (targetNodes.size() > 0 && targetCoords.size() > 0) {
+
+			final KDTree<FlagNode<RealPoint>> Tree = new KDTree<FlagNode<RealPoint>>(targetNodes, targetCoords);
+
+			final NNFlagsearchKDtree<RealPoint> Search = new NNFlagsearchKDtree<RealPoint>(Tree);
+
+
+				final RealPoint source = Clickedpoint;
+				final RealPoint sourceCoords = new RealPoint(source);
+				Search.search(sourceCoords);
+				final FlagNode<RealPoint> targetNode = Search.getSampler().get();
+
+				KDtreeroi = targetNode.getValue();
+
+		}
+
+		return KDtreeroi;
+	}
+	
 	
 	public static void main(String[] args) throws IOException {
 		
 		new ImageJ();
 		
-		String HumanCSVFile = new String ("/Users/aimachine/DocumentsVicData/DivisionMovie2.csv");
-		String AICSVFile = new String ("/Users/aimachine/DocumentsVicData/ONETDivisionMovie2.csv");
+		String HumanCSVFile = new String ("/Users/aimachine/Documents/VicData/HumanApoptosisMovie2.csv");
+		String AICSVFile = new String ("/Users/aimachine/Documents/VicData/ONETApoptosisMovie2.csv");
 		
 		// Read files
 		
 		List<RealPoint> HumanDots = loadPoints3dCsv(HumanCSVFile);
 		List<RealPoint> AIDots = loadPoints3dCsv(AICSVFile);
 		
+		double distanceThreshold = 40;
+		
+		int timeThreshold = 3;
+				
+		// Assume Human did a better job, check for AI accuracy:
+		
+		double AIaccuracy = CheckMaster(HumanDots, AIDots, distanceThreshold, timeThreshold);
+		
+		System.out.println("If Human was right AI accuracy is: " + AIaccuracy);
+		
+		// Assume Human did a better job, check for AI accuracy:
+		
+		double Humanaccuracy = CheckMaster(AIDots, HumanDots, distanceThreshold, timeThreshold);
+				
+		System.out.println("If AI was right Human accuracy is: " + Humanaccuracy);
 		
 	}
 	
